564237574ed07c5f235ac0184b1ad599c9b638b1
Abe <73765476+abedblue@users.noreply.github.com>
2021-09-19 15:05:46 -0400

    Add files via upload

---
+++ Testrun/src/testrun/Testrun.java
@@ -0,0 +1,77 @@
+import java.io.File;
+import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Rectangle2D;
+import javafx.geometry.VPos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+ 
+ 
+public class Testrun extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+
+        
+        scene = new Scene(new Browser(),750,500, Color.web("#666970"));
+        stage.setX(primaryScreenBounds.getMinX());
+        stage.setY(primaryScreenBounds.getMinY());
+        stage.setWidth(primaryScreenBounds.getWidth());
+        stage.setHeight(primaryScreenBounds.getHeight());
+        stage.setScene(scene);
+        //scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
+    }
+}
+class Browser extends Region {
+ 
+    final WebView browser = new WebView();
+    final WebEngine webEngine = browser.getEngine();
+     
+    public Browser() {
+        //apply the styles
+        getStyleClass().add("browser");
+        // load the web page
+        //webEngine.load("http://www.oracle.com/products/index.html");
+        File f = new File("src//Resources//RogueLite_1.html");
+        
+        webEngine.load(f.toURI().toString());
+        //add the web view to the scene
+        getChildren().add(browser);
+ 
+    }
+    private Node createSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        return spacer;
+    }
+ 
+    @Override protected void layoutChildren() {
+        double w = getWidth();
+        double h = getHeight();
+        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
+    }
+ 
+    @Override protected double computePrefWidth(double height) {
+        return 750;
+    }
+ 
+    @Override protected double computePrefHeight(double width) {
+        return 500;
+    }
+}
